4aae75d4c5a95b24b14ca2c5b8b6664ee9e43397
Ted Tran <TedTran2019@gmail.com>
2019-11-15 17:26:54 -0800

    Completed multiple user mentions and being able to remove them

--- ajax-twitter/app/assets/javascripts/bundle.js
+++ ajax-twitter/app/assets/javascripts/bundle.js
@@ -210,6 +210,9 @@ const Util = __webpack_require__(/*! ./api_utils.js */ "./frontend/api_utils.js"
 class TweetCompose {
 	constructor($form) {
 		this.form = $form;
+		this.form.find('select').remove();
+		this.addMention();
+		this.form.on('click', 'a.remove-mentioned-user', event => this.removeMentionedUser(event));
 		this.countRemaining();
 		this.submit();
 	}
@@ -226,9 +229,11 @@ class TweetCompose {
 
 	clearInput() {
 		this.form.find('textarea').val('');
+		$('.tweet-compose .mention').remove();
 	}
 
 	handleSuccess(tweet) {
+		console.log(tweet);
 		this.clearInput.bind(this)();
 		this.form.find(':input').prop('disabled', false);
 		let ul_name = this.form.data('tweets-ul');
@@ -245,6 +250,31 @@ class TweetCompose {
 			let input = textarea.val();
 			strong.text(`${140 - input.length}`);
 		});
+	}
+
+	newUserSelect() {
+		let div = $('<div>').addClass('mention');
+		let select = $('<select>').attr('name', "tweet[mentioned_user_ids][]");
+		div.append(select);
+		div.insertAfter('.tweet-compose textarea');
+		window.users.forEach(user => {
+			let option = $('<option>').val(user.id).text(user.username);
+			select.append(option);
+		});
+		let remove = $('<a>').addClass('remove-mentioned-user').text('remove');
+		div.append(remove);
+	}
+
+	addMention() {
+		let link = $('<a>')
+		.addClass('add-mention')
+		.text('Add mention');
+		link.insertAfter('.tweet-compose textarea');
+		link.on('click', this.newUserSelect.bind(this));
+	}
+
+	removeMentionedUser(event) {
+		$(event.currentTarget).parent().remove();
 	}
 }
 
